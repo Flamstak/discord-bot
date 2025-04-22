b269aec72f73e46424270500319339fbccc95187
Flamstak <flamstak@gmail.com>
2025-04-22 21:34:05 +0200

    podstawa bota V0.0.1

--- discord_bot.py
+++ discord_bot.py
@@ -1,7 +1,9 @@
 import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
 # Ustawienie intencji
 intents = discord.Intents.default()
 intents.message_content = True
